edf478d5536a31db12b25713fcf9bd1bf59a53f0
Bohdan Tsaplia <me@gishadev.com>
2025-07-02 23:28:01 +0300

    add primeTween

--- ProjectSetup.cs
+++ ProjectSetup.cs
@@ -43,6 +43,7 @@ namespace gishadev
         [MenuItem("Tools/Setup/Import polishing tools")]
         static void ImportPolishingTools()
         {
+            Assets.ImportAsset("PrimeTween · High-Performance Animations and Sequences.unitypackage", "Plugins/PrimeTween");
             Packages.InstallPackages(new[]
             {
                 "https://github.com/hadashiA/VContainer.git?path=VContainer/Assets/VContainer#1.16.9",
